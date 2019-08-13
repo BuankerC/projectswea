d6aa4b9b974f4992d68a4aaf9a3691be8e83a3c8
Yang Seon <ergaster91@gmail.com>
2019-08-14 08:48:46 +0900

    20190813 | exercise

---
+++ swea 1208.py
@@ -0,0 +1,10 @@
+for tc in range(1, 11):
+    num = int(input())
+    Data = list(map(int, input().split()))
+    for i in range(num):
+        max_d, min_d = max(Data), min(Data)
+        index_max_d = Data.index(max_d)
+        index_min_d = Data.index(min_d)
+        Data[index_max_d] -= 1
+        Data[index_min_d] += 1
+    print('#%s %d'%(tc, max(Data) - min(Data)))
