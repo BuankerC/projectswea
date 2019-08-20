5d6a38dda60ff76ba3ffacb96c41709e4c6cbbd4
Yang Seon <ergaster91@gmail.com>
2019-08-20 19:58:41 +0900

    20190820 | 야자

---
+++ 20190820/swea 2007 patterns.py
@@ -0,0 +1,8 @@
+tc = int(input())
+for t in range(1, tc+1):
+    string = input()
+    for i in range(1, len(string)):
+        if string[0:i] == string[i:2*i]:
+            ans = i
+            break
+    print('#%d %d' % (t, ans))
