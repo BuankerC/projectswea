f52a64c8b275f0e008ef1d49b383406a62166fd4
Yang Seon <ergaster91@gmail.com>
2019-08-27 15:41:32 +0900

    20190827 | plus problems

--- 20190827/모의1.py
+++ 20190827/모의1.py
@@ -1,3 +1,15 @@
 T = int(input())
-N = map(int, input().split())
-for i in range(N):
+# N = map(int, input().split())
+for i in range(1, T + 1):
+    N = int(input())
+    prices = list(map(int, input().split()))
+
+    purchase_price = -1000
+
+    for j in range(len(prices)):
+        if min(prices[:-1]) == prices[j]:
+            purchase_price = j
+    selling_price = prices[purchase_price+1:]
+
+    maximum_profit = max(selling_price) - prices[j]
+    print('#{} {}'.format(i, maximum_profit))
