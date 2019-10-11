53549be6fcbcbe349229eee3e783ae0e5fec9ca2
Seon Yang <ergaster91@gmail.com>
2019-10-11 14:52:11 +0900

    20191011 | swea 3750

---
+++ 20191011/swea 3750.py
@@ -0,0 +1,16 @@
+'''
+swea 3750. Digit sum D3
+'''
+
+res = []
+tc = int(input())
+for t in range(tc):
+    num = input()
+    while len(num) > 1:
+        tmp = 0
+        for n in num:
+            tmp += int(n)
+        num = str(tmp)
+    res.append(num)
+for t in range(tc):
+    print('#{} {}'.format(t+1, res[t]))
